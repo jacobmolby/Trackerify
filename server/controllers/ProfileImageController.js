68cff50ad6f80d7c105a0815a96a4139ecbf4d88
Jacob Mølby <jacob.moelby@gmail.com>
2020-02-11 23:15:45 +0100

    Add delete old profile image from S3

--- server/controllers/ProfileImageController.js
+++ server/controllers/ProfileImageController.js
@@ -25,17 +25,32 @@ module.exports = {
       }
       const returnData = {
         signedRequest: data,
-        imageUrl: `https://${S3_BUCKET}.s3-eu-west-1.amazonaws.com/profile-images/${hash}-${fileName}`
+        imageUrl: `https://${S3_BUCKET}.s3-eu-west-1.amazonaws.com/profile-images/${hash}-${fileName}`,
+        imageKey: `${hash}-${fileName}`
       };
       res.send(returnData);
     });
   },
   async update(req, res) {
     const { imageUrl } = req.body;
+    const s3 = new aws.S3();
+
     try {
       const user = await User.findById(req.user._id);
+      const oldImageUrl = user.profileImage;
       user.profileImage = imageUrl;
       await user.save();
+
+      const imageKey = oldImageUrl.split('s3-eu-west-1.amazonaws.com/')[1];
+      const params = {
+        Bucket: S3_BUCKET,
+        Key: imageKey
+      };
+      //Deletes the old image from the S3 bucket
+      s3.deleteObject(params, function(err, data) {
+        if (err) console.log(err, err.stack);
+      });
+
       res.send({ message: 'Succes' });
     } catch (error) {
       res.status(400).send({ error: error.message });
